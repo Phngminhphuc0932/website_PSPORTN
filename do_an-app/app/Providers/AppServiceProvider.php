8aba977538443b1c8b5aa7a69e1913e5f2c39c62
Phngminhphuc0932 <62290570+Phngminhphuc0932@users.noreply.github.com>
2022-10-08 20:17:47 +0700

    đã xong danh mục sản phẩm

--- do_an-app/app/Providers/AppServiceProvider.php
+++ do_an-app/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,25 +27,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $list_loai_sp = DB::table('sb_loai_san_pham')
+        ->where('id_loai_cha', 0)
+        ->get();
 
-        /*load gio hang cho tat ca*/
-        // $tong_so_luong = 0;
-        // if(Session::has('gio_hang')){
-        //     $gio_hang = Session::get('gio_hang');
-        //     for ($i = 0; $i < count($gio_hang); $i++){
-        //         $tong_so_luong += $gio_hang[$i]->so_luong;
-        //     }
-        // }
+        $list_loai_sp = json_decode(json_encode($list_loai_sp));
 
-        // View::share('tong_so_luong', $tong_so_luong);
-        // $gio_hang = [];
-        // if(Session::has('gio_hang')){
-        //     $gio_hang = Session::get('gio_hang');
+        foreach($list_loai_sp as $key => $loai_sp_cha){
+            // echo $loai_sp_cha->ID_loai_sp. " ";
+            $list_ds_loai_con = DB::table('sb_loai_san_pham')
+            ->where('id_loai_cha', $loai_sp_cha->ID_loai_sp)
+            ->get();
 
-        // }
+            $list_ds_loai_con = json_decode(json_encode($list_ds_loai_con));
 
-        // View::share('gio_hang', $gio_hang);
+            // echo '<pre>',print_r($list_ds_loai_con) ,'</pre>';
+
+            $list_loai_sp[$key]->ds_loai_con = $list_ds_loai_con;
+        }
+
+        View::share('ds_loai_sp', $list_loai_sp);
 
         Blade::directive('VND_monney', function ($money) {
             return "<?php echo number_format($money); ?>";
--- do_an-app/resources/views/modules/mod_sub_cate.blade.php
+++ do_an-app/resources/views/modules/mod_sub_cate.blade.php
@@ -2,52 +2,21 @@
     <div class=" top-nav rsidebar span_1_of_left">
         <h3 class="cate">DANH MỤC </h3>
         <ul class="menu">
-            <li class="item1"><a href="#">NỔI BẬT<img class="arrow-img" src="images/arrow1.png"
-                        alt="" /> </a>
-                <ul class="cute">
-                    <li class="subitem1"><a href="/product">NIKE </a></li>
-                    <li class="subitem2"><a href="/product">JORDAN </a></li>
-                    <li class="subitem3"><a href="/product">PEAK SPORT </a></li>
-                </ul>
-            </li>
-            <li class="item2"><a href="#">GIÀY DÉP<img class="arrow-img " src="images/arrow1.png"
-                        alt="" /></a>
-                <ul class="cute">
-                    <li class="subitem1"><a href="/product">BASKETBALL </a></li>
-                    <li class="subitem2"><a href="/product">SNEAKER </a></li>
-                    <li class="subitem3"><a href="/product">RUNNING </a></li>
-                    <li class="subitem3"><a href="/product">SLIDE & SANDALS </a></li>
-                    <li class="subitem3"><a href="/product">PEAK SPORT </a></li>
-                </ul>
-            </li>
-            <li class="item3"><a href="#">QUẦN ÁO<img class="arrow-img img-arrow" src="images/arrow1.png"
-                        alt="" /> </a>
-                <ul class="cute">
-                    <li class="subitem1"><a href="/product">Cute Kittens </a></li>
-                    <li class="subitem2"><a href="/product">Strange Stuff </a></li>
-                    <li class="subitem3"><a href="/product">Automatic Fails</a></li>
-                </ul>
-            </li>
-            <li class="item4"><a href="#">PHỤ KIỆN <img class="arrow-img img-left-arrow"
-                        src="images/arrow1.png" alt="" /></a>
-                <ul class="cute">
-                    <li class="subitem1"><a href="/product">PROCOMBAT</a></li>
-                    <li class="subitem2"><a href="/product">SOCKS </a></li>
-                    <li class="subitem3"><a href="/product">BALL </a></li>
-                    <li class="subitem3"><a href="/product">BACKPACKS </a></li>
-                    <li class="subitem3"><a href="/product">OTHERS </a></li>
-                </ul>
-            </li>
-            <li class="item4"><a href="#">SALES<img class="arrow-img img-left-arrow"
-                        src="images/arrow1.png" alt="" /></a>
-                <ul class="cute">
-                    <li class="subitem1"><a href="/product">FOOTWARE SALES </a></li>
-                    <li class="subitem2"><a href="/product">APPAREL SALES </a></li>
-                    <li class="subitem3"><a href="/product">ACESSORIES SALES </a></li>
-                </ul>
-            </li>
+            @for ($i = 0; $i < count($ds_loai_sp); $i++)
+                <li @if (count($ds_loai_sp[$i]->ds_loai_con) > 0) class="item1" @endif><a
+                        href="#">{{ $ds_loai_sp[$i]->ten_loai_sp }}<img class="arrow-img" src="images/arrow1.png"
+                            alt="" /></a>
+                    @if (count($ds_loai_sp[$i]->ds_loai_con) > 0)
+                        <ul class="cute">
+                            @for ($j = 0; $j < count($ds_loai_sp[$i]->ds_loai_con); $j++)
+                                <li class="subitem1"><a
+                                        href="#">{{ $ds_loai_sp[$i]->ds_loai_con[$j]->ten_loai_sp }}</a></li>
+                            @endfor
+                        </ul>
+                    @endif
+                </li>
+            @endfor
             <ul class="kid-menu ">
-                <li><a href="/product">SHOES CARE</a></li>
                 <li class="menu-kid-left"><a href={{ url('contact') }}>CONTACT US</a></li>
             </ul>
         </ul>
